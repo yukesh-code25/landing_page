c74783fdbf91054fcca49b195d629b8b99b995d7
yukesh-code25 <yukesh.codee@gmail.com>
2026-09-05 11:25:03 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
+  
 import Navbar from './components/Navbar.jsx'
 import HeroSection from './section/HeroSection.jsx'
 import About from './section/About.jsx'
 import Stats from './section/Stats.jsx'
-import Dishes from './section/dishes.jsx'
+import Dishes from './section/Dishes.jsx'
 import Features from './section/Features.jsx'
 import BookingProcess from './section/BookingProcess.jsx'
 import Timing from './section/Timing.jsx'
 import TestimonialSection from './section/TestimonialSection.jsx'
 import FAQs from './section/FAQs.jsx'
-import CTA from './section/cta.jsx'
+import CTA from './section/CTA.jsx'
 import Footer from './components/Footer.jsx'
 import LenisScroll from './components/LenisScroll.jsx'
 
